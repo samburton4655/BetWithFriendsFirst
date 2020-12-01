3ad951e8334aba38d7abdb8b6a6fa7128d9c9e4f
Sam Burton <samburton4655@gmail.com>
2020-11-30 20:14:10 -0500

    Initial Commit

---
+++ BetWithFriendsFirst/BetWithFriendsFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetWithFriendsFirstApp.swift
+//  BetWithFriendsFirst
+//
+//  Created by Samuel Burton on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct BetWithFriendsFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetWithFriendsFirst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetWithFriendsFirst
+//
+//  Created by Samuel Burton on 11/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
